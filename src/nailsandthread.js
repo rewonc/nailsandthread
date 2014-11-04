f53e1c82710b3bfc66c13a28bbd46e1f2256cad4
rewonc <rewonc@gmail.com>
2014-11-04 14:57:46 -0800

    Add undefined functions

---
+++ src/nailsandthread.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Parser = {
+  getRGB: function(){
+
+  }
+};
+
+var Grid = {
+  generate: function(options) {
+
+  },
+  findNextPoint: function(origin, grid, pixels, color){
+
+  }
+};
+
+var Canvas = {
+  generate: function(grid){
+
+  },
+  render: function(canvas, $element){
+
+  }
+};
