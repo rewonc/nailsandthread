15572b9e816b52803d918bce11726ac7e4dcb5c8
rewonc <rewonc@gmail.com>
2014-11-05 08:17:27 -0800

    selectNext, getRandom, checkPopulated subroutines in findNextPoint

--- src/nailsandthread.js
+++ src/nailsandthread.js
@@ -28,12 +28,34 @@ var Grid = {
     grid.width = options.width;
     grid.height = options.height;
     for(var i=0;i<grid.size;i++){
-      grid.rows[i] = Array(grid.size);
+      grid.rows[i] = new Array(grid.size);
     }
     return grid;
   },
-  findNextPoint: function(origin, grid, pixels, color){
-
+  findNextPoint: function(origin, grid, pixels, color, thickness){
+    var result = "next: " + selectNext(origin, grid);
+    //randomly select the next point
+    //check if it is populated(if so, restart)
+    //check if it is feasible(if not, restart)
+    //if restart more than 10 times, choose another path
+    //else, decrement the value of all the arrays
+    //return the next node
+    return result;
+    function getRandom(grid){
+      return Math.floor(Math.random()*grid.size);
+    }
+    function checkPopulated(origin, target){
+      if (origin === target) return true;
+      if (grid.rows[origin][target] && color in grid.rows[origin][target]) return true;
+      return false;
+    }
+    function selectNext(origin, grid, counter){
+      counter = counter || 0;
+      if(counter > 10) return false;
+      var x = getRandom(grid);
+      if(!checkPopulated(origin, x)) return x;
+      return selectNext(origin, grid, counter + 1);
+    }
   }
 };
 
